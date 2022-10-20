40edbdb82bf6ee2fcf64522538be9d074eef04d8
Gino Murin <gmurin@gmail.com>
2022-10-19 21:24:32 -0400

    completed navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import {Switch, Route, Link} from 'react-router-dom'
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import AddReview from './components/add-review';
+import Movie from './components/movie';
+import MovieList from './components/movies-list';
+import Login from './components/login';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  async function login(user=null){
+    setUser(user)
+  }
+
+  async function logout(){
+    setUser(null)
+  }
   return (
     <div className="App">
-      Hello World
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">
+              <Link to={'/movies'}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link href="#link">
+              { user ? (<button onClick={logout}>Logout</button>) : (<Link to={'/login'}>Login</Link>)}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
